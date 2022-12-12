fc89b16eaeb405d2ae10ee14156493ce9e878a33
arifthalhah <65509122+arifth@users.noreply.github.com>
2022-12-13 01:54:28 +0700

    create jumbotron ,navbar comp

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Button } from 'react-bootstrap';
 
 function NavBar() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
+      <Navbar style={{background:"transparent",top:0, position:"absolute",zIndex:999, width:"100vw"}} >
+        <Container className='d-flex align-items-center' >
+          <Navbar.Brand href="/">
             <img
               alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            React Bootstrap
+              src="/icon.png"
+              width="190"
+              height="68"
+              className="d-inline-block align-top "
+            />
           </Navbar.Brand>
+          <div style={{alignSelf: "end" }} className="d-flex align-item-center mb-3">
+          <Button  className='me-3 text-white' style={{width: "6rem", background: "transparent", border: "1px solid white"}}>Register</Button>
+          <Button  className='bg-primary text-white' style={{width: "6rem"}} >Login</Button>
+          </div>
         </Container>
       </Navbar>
     </>
